4a5ec92696a7fbee3b4030a5fee1c30b1b4318eb
MCNaOtlichnoYT <105880094+MCNaOtlichnoYT@users.noreply.github.com>
2026-08-26 21:17:36 +0300

    Create install.lua

---
+++ install.lua
@@ -0,0 +1,8 @@
+print('Ship Control Protocol v 2.1')
+print('Installing: 0%')
+wget https://raw.githubusercontent.com/MCNaOtlichnoYT/ShipControlProtocol/refs/heads/main/scp_functions.lua scp_functions.lua
+print('Installing: 33%')
+wget https://raw.githubusercontent.com/MCNaOtlichnoYT/ShipControlProtocol/refs/heads/main/scp_config.lua scp_config.lua
+print('Installing: 67%')
+wget https://raw.githubusercontent.com/MCNaOtlichnoYT/ShipControlProtocol/refs/heads/main/panel.lua
+print('Installing: 100%')
